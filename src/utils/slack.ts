c2d03e28bfd41fc25f00a277ce5f504bc08e3d32
Lorenz Ras <lenzras@gmail.com>
2022-03-10 16:14:23 +0900

    Update slack.ts

--- src/utils/slack.ts
+++ src/utils/slack.ts
@@ -1,6 +1,7 @@
 import {slackApp} from '../api/slack';
 
 export const notifyDev = (message: string) => {
+  console.log(process.env.SLACK_CHANNEL_ID_DEV);
   slackApp.client.chat.postMessage({
     channel: process.env.SLACK_CHANNEL_ID_DEV,
     text: message,
