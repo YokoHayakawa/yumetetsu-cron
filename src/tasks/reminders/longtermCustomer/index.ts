06ee1f10305087f36438647d1d3b55bac53da37c
Lorenz Ras <lenzras@gmail.com>
2022-03-11 09:56:56 +0900

    Update index.ts

--- src/tasks/reminders/longtermCustomer/index.ts
+++ src/tasks/reminders/longtermCustomer/index.ts
@@ -1,6 +1,6 @@
 import {format} from 'date-fns';
 import {LongTermCustomerType} from '../../../types/kintone';
-import {notifyDev} from '../../../utils';
+import {formattedTime, notifyDev} from '../../../utils';
 import getLongTermCust from './lib/getLongTermCust';
 import sendToSlack from './lib/sendToSlack';
 
@@ -10,8 +10,8 @@ import sendToSlack from './lib/sendToSlack';
 export const longtermCustomer = async () => {
   const result = await getLongTermCust();
   // eslint-disable-next-line max-len
-  notifyDev(`Cron job: longtermCustomer ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}\nRecords: ${result.totalCount}`);
-  // console.log(process.env.NODE_ENV, result.records.length, 'node');
+  notifyDev(`${formattedTime} Task: longtermCustomer \nRecords: ${result.totalCount}`);
+
   if (result.ok) {
     await sendToSlack(
       result.records as unknown as LongTermCustomerType[],
