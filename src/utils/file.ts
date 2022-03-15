ae141e07d64a413ab404951983faa9352a7e9a9b
Lorenz Ras <lenzras@gmail.com>
2022-03-15 13:24:19 +0900

    initialized csv datetime fixer.

---
+++ src/utils/file.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Fix datetime of provided fields to match kintone format.
+ * Kintone format: yyyy-MM-dd HH:mm
+ * @param path
+ * @param fields
+ */
+const fixCSVDateTime = (path: string, fields: string[]) => {
+  console.log(path);
+};
