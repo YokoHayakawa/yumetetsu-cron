c3b4616fa3f5854c4ef3920519248b5dc93acd06
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-04-21 14:58:19 +0900

    update

--- src/tasks/syncs/portalCheck/main.test.ts
+++ src/tasks/syncs/portalCheck/main.test.ts
@@ -2,4 +2,4 @@ import {main} from './main';
 
 test('main', async () => {
   expect(await main()).toMatchSnapshot();
-}, 10000000000);
+}, 100000);
--- src/tasks/syncs/portalCheck/main.ts
+++ src/tasks/syncs/portalCheck/main.ts
@@ -1,24 +1,55 @@
-import {openBrowserPage} from '../../common/browser';
+import {openBrowserPage, openMockBrowserPage} from '../../common/browser';
+
+const DEBUG_MODE = true;
 
 export const main = async () => {
-  const firstPage = await openBrowserPage();
-  await firstPage.goto('https://suumo.jp/ms/chuko/aichi/city/');
-  await firstPage.click('#sa02_sc201');
+  let firstPage;
+  if (DEBUG_MODE) {
+    firstPage = await openMockBrowserPage();
+  } else {
+    firstPage = await openBrowserPage();
+    await firstPage.goto('https://suumo.jp/ms/chuko/aichi/city/');
+    await firstPage.click('#sa02_sc201');
 
-  await Promise.all([
-    firstPage.click('.js-searchBtn'),
-    firstPage.waitForNavigation(),
-  ]);
+    await Promise.all([
+      firstPage.click('.js-searchBtn'),
+      firstPage.waitForNavigation(),
+    ]);
 
-  await Promise.all([
-    firstPage.click('.ui-icon--tabview'),
-    firstPage.waitForNavigation(),
-  ]);
+    await Promise.all([
+      firstPage.click('.ui-icon--tabview'),
+      firstPage.waitForNavigation(),
+    ]);
+  }
 
-  const bukkenmei = await firstPage.$$eval('.property_unit-title_wide',
-    (el:any) => el.map((data:any) => data.innerText));
-  console.log(bukkenmei);
+  const bukkenlists = await firstPage.$$eval(
+    '.property_unit.property_unit--osusume2',
+    (el: any) => el.map((data: any) => {
+      console.log('data', data);
+      console.log(data.getElementsByClassName('property_unit-title_wide')[0]
+        .getElementsByTagName('a')[0].href);
+
+      return {
+        bukkenmei: data
+          .getElementsByClassName('property_unit-title_wide')[0].innerText,
+        kakaku: data.getElementsByClassName('dottable-value--2')[0].innerText,
+        shozaichi: data.getElementsByTagName('dd')[2].innerText,
+        menseki: data.getElementsByTagName('dd')[1].innerText,
+        URL: data.getElementsByClassName('property_unit-title_wide')[0]
+          .getElementsByTagName('a')[0].href,
+      };
+    }));
+
+
+  console.log(bukkenlists.length);
 
   await firstPage.waitForTimeout(2000);
-  await firstPage.browser().close();
+
+  if (DEBUG_MODE) {
+    await firstPage.browser().disconnect();
+  } else {
+    await firstPage.browser().close();
+  }
+
+  return bukkenlists;
 };
